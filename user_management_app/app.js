609599bdf5a8f5a0497fbc16de356b9652ca4b18
Jolanda Keizer <info@gevalapart.nl>
2017-05-12 12:58:39 +0200

    part 2

--- user_management_app/app.js
+++ user_management_app/app.js
@@ -1,16 +1,31 @@
 var express = require('express');
 var fs = require('fs');
 var app = express();
-const bodyParser = require('body-parser')
-
-app.use('/', bodyParser())
-
+const bodyParser = require('body-parser');
 
 app.set('views', 'src/views');
 app.set('view engine', 'pug');
 
+// app.use('/', bodyParser())
 
-app.post ('/', function(request, response, next) {
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+// renders a page with three forms on it (first name, last name, and email) 
+// that allows you to add new users to the users.json file.
+
+app.get('/', function(request, response) {
+    response.render("index")
+});
+
+
+
+
+// takes in the post request from your form, then displays matching users on a new page. 
+// Users should be matched based on whether either their first or last name contains the input string.
+
+app.post ('/', function(request, response) {
     fs.readFile('./resources/users.json', function(err, data) {
         var parsedData = JSON.parse(data);
         console.log("parsedData app.post('/')");
@@ -18,33 +33,26 @@ app.post ('/', function(request, response, next) {
         console.log("request.body app.post('/')")
         console.log(request.body)
 
+        let result = []    
 
         for (var i = 0; i < parsedData.length; i++){
-            //am not allowed to sent multiple responses -- TO DO
-            if(parsedData[i].firstName === request.body.firstName || parsedData[i].lastName === request.body.lastName){
-                response.end("Name found")
+
+
+            firstName = request.body.firstName
+            lastName = request.body.lastName
+
+            if(parsedData[i].firstName === firstName || parsedData[i].lastName === lastName){
+                result.push(parsedData[i])
             }
-        } 
-        response.render("form", 
-        {
-            users: parsedData
-        })
-    })    
-})
+             console.log(result);
 
-app.get('/', function(request, response) {
-    fs.readFile('./resources/users.json', function(err, data) {
-        if (err) {
-            console.log(err);
-        }
-
-        var parsedData = JSON.parse(data);
-        console.log("parsedData app.get('/')");
-        console.log(parsedData);
-        response.render("index", {
-            users: parsedData
+             response.render("founduser", {
+            foundUsers: result
         });
-    });
+        } 
+       
+        
+    });   
 });
 
 var server = app.listen(3000, function() {
